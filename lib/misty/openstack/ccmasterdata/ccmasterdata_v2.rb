6b6f2300844671442d416bbdbabb84988079a421
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-08-23 09:57:22 +0000

    rename solutions

--- lib/misty/openstack/ccmasterdata/ccmasterdata_v2.rb
+++ lib/misty/openstack/ccmasterdata/ccmasterdata_v2.rb
@@ -5,7 +5,7 @@ module Misty::Openstack::CcmasterdataV2
       "/masterdata/domains/{domain_id}" => {:GET=>[:get_domain], :PUT=>[:set_domain]},
       "/masterdata/inheritance/"  => {:GET=>[:inheritance]},
       "/masterdata/missing/"  => {:GET=>[:get_missing_projects]},
-      "/masterdata/solutions/" => {:GET=>[:solutions]}
+      "/masterdata/solutions/" => {:GET=>[:get_solutions]}
     }
   end
 end
